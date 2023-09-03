556084a93edf4c61e8f73b1d64394562cd62b8f3
KSDeshappriya <ksdeshappriya.official@gmail.com>
2023-09-03 13:04:21 +0530

    typeAnim

--- assest/js/script.js
+++ assest/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     window.onscroll = () => {
         const scrollY = window.scrollY;
         const position = 0;
-    
+
         if (scrollY > position) {
             cursorHighlight.style.position = "fixed";
         }
@@ -22,3 +22,49 @@ document.addEventListener('DOMContentLoaded', function () {
         cursorHighlight.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
     });
 });
+
+
+// // typing-effect.js
+// class TypingEffect {
+//     constructor(element, textArray, speed, eraseDelay) {
+//         this.element = element;
+//         this.textArray = textArray;
+//         this.speed = speed;
+//         this.eraseDelay = eraseDelay;
+//         this.currentIndex = 0;
+//         this.isTyping = false;
+//     }
+
+//     async type() {
+//         if (this.isTyping) return;
+//         this.isTyping = true;
+//         for (const text of this.textArray) {
+//             await this.typeText(text);
+//             await this.eraseText();
+//         }
+//         this.isTyping = false;
+//     }
+
+//     async typeText(text) {
+//         for (const char of text) {
+//             this.element.textContent += char;
+//             await new Promise(resolve => setTimeout(resolve, this.speed));
+//         }
+//     }
+
+//     async eraseText() {
+//         await new Promise(resolve => setTimeout(resolve, this.eraseDelay));
+//         const text = this.element.textContent;
+//         while (text.length > 0) {
+//             this.element.textContent = text.slice(0, -1);
+//             await new Promise(resolve => setTimeout(resolve, 50));
+//         }
+//     }
+// }
+
+// // Example usage:
+// const ele = document.querySelector('#type');
+// const textArray = ['Want to learn Coding?', 'Want to learn '];
+// const typingEffect = new TypingEffect(ele, textArray, 50, 30);
+// typingEffect.type();
+
